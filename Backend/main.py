24e8f7950228da97721648cbb6ff180f31460076
maximilianochiba <mchiba@fi.uba.ar>
2024-07-22 01:42:51 -0300

    Creacion de la Db y principios del index

---
+++ Backend/main.py
@@ -0,0 +1,174 @@
+
+from flask import Flask, request, jsonify
+from models import db, Equipos, Jugadores
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+port = 5000
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:fumi1016@localhost:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/equipos', methods=['GET'])
+def get_equipos():
+    try: 
+        equipos = Equipos.query.order_by(Equipos.nombre).all()
+        #print(equipos)
+        equipos_data = []
+        for equipo in equipos:
+            equipo_data = {
+                'id': equipo.id,
+                'nombre': equipo.nombre,
+                'titulos': equipo.titulos,
+                'estadio': equipo.estadio
+            }
+            equipos_data.append(equipo_data)
+        return jsonify({'equipos': equipos_data})
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+
+
+@app.route('/jugadores', methods=['GET'])
+def get_jugadores2():
+    try: 
+        jugadores = Jugadores.query.order_by(Jugadores.nombre).all()
+        #print(equipos)
+        jugadores_data = []
+        for juagador in jugadores:
+            jugador_data = {
+                'id': jugador.id,
+                'id_equipo': jugador.id_equipo,
+                'nombre': jugador.nombre,
+                'edad': jugador.edad,
+                'altura': jugador.altura,
+                'posicion': jugador.posicion,
+                'nacionalidad': jugador.nacionalidad
+            }
+            jugadores_data.append(jugador_data)
+        return jsonify({'jugadores': jugadores_data})
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+
+
+@app.route('/equipos/<id_equipo>', methods=['GET'])
+def data(id_equipo):
+    try: 
+        equipo = Equipos.query.get(id_equipo)
+        #print(equipos)       
+        equipo_data = {
+           #'id': equipo.id,
+            'nombre': equipo.nombre,
+            'titulos': equipo.titulos,
+            'estadio': equipo.estadio
+        }
+        return equipo_data
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+
+@app.route('/equipos/<id_equipo>/jugadores', methods=['GET'])
+def data2(id_equipo):
+    try: 
+        equipo = Equipos.query.get(id_equipo)
+        #print(equipos) 
+        equipo_data = {
+            #'nombre': equipo.nombre,
+            #'id': equipo.id,
+            #'titulos': equipo.titulos,
+            #'estadio': equipo.estadio,
+            'jugadores': []
+            
+        }
+        for jugador in equipo.jugadores:      
+            jugador_data = {
+                'id': jugador.id,
+                'id_equipo': jugador.id_equipo,
+                'nombre': jugador.nombre,
+                'edad': jugador.edad,
+                'altura': jugador.altura,
+                'posicion': jugador.posicion,
+                'nacionalidad': jugador.nacionalidad
+            }
+            equipo_data['jugadores'].append(jugador_data)
+        return equipo_data 
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+
+
+@app.route('/equipos/<id_equipo>/jugadores', methods=['POST'])
+def nuevo_jugador(id_equipo):
+    try: 
+        equipo = Equipos.query.get(id_equipo)
+
+        data = request.json
+        id_equipo = data.get('id_equipo')
+        nombre = data.get('nombre')
+        edad = data.get('edad')
+        altura = data.get('altura')
+        posicion = data.get('posicion')
+        nacionalidad = data.get('nacionalidad')
+        if not id_equipo or not nombre or not edad or not altura or not posicion or not nacionalidad:
+            return jsonify({'message': 'Bad request, isbn or name or cantPages or author not found'}), 400
+        new_jugador = Jugador(id_equipo=id_equipo, nombre=nombre, edad=edad, altura=altura, posicion=posicion, nacionalidad=nacionalidad)
+        db.session.add(new_jugador)
+        db.session.commit()
+        return jsonify({'jugador': {'id': new_jugador.id, 'id_equipo': new_jugador.id_equipo, 'nombre': new_jugador.nombre, 'edad': new_jugador.edad, 
+        'altura': new_jugador.altura, 'posicion': new_jugador.posicion, 'nacionalidad': new_jugador.nacionalidad}}), 201
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+
+
+
+
+
+@app.route('/equipos/river', methods=['GET'])
+def get_jugadores():
+    try: 
+        jugadores = Jugadores.query.where(Jugadores.id_equipo == 1).all()
+        #print(equipos)
+        jugadores_data = []
+        for jugador in jugadores:
+            jugador_data = {
+                'id': jugador.id,
+                'id_equipo': jugador.id_equipo,
+                'nombre': jugador.nombre,
+                'edad': jugador.edad,
+                'altura': jugador.altura,
+                'posicion': jugador.posicion,
+                'nacionalidad': jugador.nacionalidad
+            }
+            jugadores_data.append(jugador_data)
+        return jsonify({'jugadores': jugadores_data})
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('Starting server...')
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    print('Started...')
+    app.run(host='0.0.0.0', debug=True, port=port)
---
+++ Backend/models.py
@@ -0,0 +1,22 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Equipos(db.Model):
+    __tablename__ = 'equipos'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    titulos = db.Column(db.Integer, nullable=False)
+    estadio = db.Column(db.String(100), nullable=False)
+    jugadores = db.relationship("Jugadores")
+
+class Jugadores(db.Model):
+    __tablename__ = 'jugadores'
+    id = db.Column(db.Integer, primary_key=True)
+    id_equipo = db.Column(db.Integer, db.ForeignKey('equipos.id'))
+    nombre = db.Column(db.String(255), nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    altura = db.Column(db.Integer, nullable=False)
+    posicion = db.Column(db.String(255), nullable=False)
+    nacionalidad = db.Column(db.String(255), nullable=False)
